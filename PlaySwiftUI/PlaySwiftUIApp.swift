6a3114e756fc47960b3612ea870e6612c8e7a839
Aidan.Lu <Aidan.Lu@inctw.com.tw>
2020-12-21 21:50:24 +0800

    Initial Commit

---
+++ PlaySwiftUI/PlaySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaySwiftUIApp.swift
+//  PlaySwiftUI
+//
+//  Created by apple on 2020/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PlaySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
